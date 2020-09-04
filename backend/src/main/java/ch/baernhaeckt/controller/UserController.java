35ed68402931ed1d4d88c279baa0b3282051bf87
timmmmmb <frey.tim@outlook.com>
2020-09-05 01:48:34 +0200

    added more to the pom and a resources file

--- backend/src/main/java/ch/baernhaeckt/controller/UserController.java
+++ backend/src/main/java/ch/baernhaeckt/controller/UserController.java
@@ -1,13 +1,39 @@
 package ch.baernhaeckt.controller;
 
+import ch.baernhaeckt.model.User;
 import ch.baernhaeckt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping
+    public @ResponseBody
+    User[] list() {
+        ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
+        return users.toArray(new User[0]);
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody
+    User getById(@PathVariable String id) {
+        return userRepository.findById(id).get();
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public User getByEmail(@RequestParam String email) {
+        return userRepository.findByEmailIgnoreCase(email).get(0);
+    }
 }
--- backend/src/main/java/ch/baernhaeckt/repository/UserRepository.java
+++ backend/src/main/java/ch/baernhaeckt/repository/UserRepository.java
@@ -4,6 +4,9 @@ import ch.baernhaeckt.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends CrudRepository<User, String> {
+    List<User> findByEmailIgnoreCase(String emails);
 }
