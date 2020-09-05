b89d72eca5dea1d8c865592c42063f42b1c8e78d
Julian Haldimann <julian@praxishaldimann.ch>
2020-09-05 08:42:45 +0200

    Add new route and fix broken css

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {RegionsComponent} from './components/regions/regions.component';
 import {HomeComponent} from './components/home/home.component';
 import {OffersComponent} from './components/offers/offers.component';
 import {UserComponent} from './components/user/user.component';
+import {RegionOfferComponent} from './components/region-offer/region-offer.component';
 
 
 const routes: Routes = [
+  { path: 'regions-detail', component: RegionOfferComponent},
   { path: 'regions', component: RegionsComponent},
   { path: 'offers', component: OffersComponent},
   { path: 'user', component: UserComponent},
