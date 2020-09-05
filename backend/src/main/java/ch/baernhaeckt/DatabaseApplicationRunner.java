b5c1322c1228bf84367a9bb55f8f33893ca546aa
timmmmmb <frey.tim@outlook.com>
2020-09-05 19:37:01 +0200

    added some comments

--- backend/src/main/java/ch/baernhaeckt/DatabaseApplicationRunner.java
+++ backend/src/main/java/ch/baernhaeckt/DatabaseApplicationRunner.java
@@ -86,7 +86,7 @@ public class DatabaseApplicationRunner implements ApplicationRunner {
                 );
     }
 
-    public static Date parseDate(String date) {
+    private static Date parseDate(String date) {
         try {
             return new SimpleDateFormat("yyyy-MM-dd").parse(date);
         } catch (ParseException e) {
