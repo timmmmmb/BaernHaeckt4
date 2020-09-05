4fd4c5abe4899f2618fc578452fa91c38b455361
timmmmmb <frey.tim@outlook.com>
2020-09-05 09:01:59 +0200

    added region and offer endpoint but havent added testdate yet

---
+++ backend/src/main/java/ch/baernhaeckt/controller/OfferController.java
@@ -0,0 +1,29 @@
+package ch.baernhaeckt.controller;
+
+import ch.baernhaeckt.model.Offer;
+import ch.baernhaeckt.model.Product;
+import ch.baernhaeckt.repository.OfferRepository;
+import ch.baernhaeckt.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/offers")
+public class OfferController {
+    @Autowired
+    private OfferRepository offerRepository;
+
+    @GetMapping
+    public @ResponseBody
+    Offer[] list() {
+        ArrayList<Offer> offers = (ArrayList<Offer>) offerRepository.findAll();
+        return offers.toArray(new Offer[0]);
+    }
+
+}
---
+++ backend/src/main/java/ch/baernhaeckt/repository/OfferRepository.java
@@ -0,0 +1,10 @@
+package ch.baernhaeckt.repository;
+
+
+import ch.baernhaeckt.model.Offer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "offer", path = "offer")
+public interface OfferRepository  extends CrudRepository<Offer, String> {
+}
